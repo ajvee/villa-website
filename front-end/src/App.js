3a59e45821d455830d7cb0012239b59f1dc71079
Angel <EMAIL>
2024-01-02 15:12:06 -0500

    frontend shell

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar"
+import Home from "./Pages/Home"
+// import Index from "./Pages/Index"
+// import Show from "./Pages/Show"
+// import Edit from "./Pages/Edit"
+// import New from "./Pages/New"
+import Error from "./Pages/Error"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Angel Website
-      </header>
+      <h1>Welcome to my personal Website</h1>
+      <Router>
+        <NavBar />
+      </Router>
     </div>
   );
 }
